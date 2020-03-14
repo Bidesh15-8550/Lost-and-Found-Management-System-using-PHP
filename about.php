b3dc97d1922dba2f9e8651b29e5e70976825bf0e
Bidesh15-8550 <bideshbiswas@ymail.com>
2020-03-14 15:43:49 +0600

    updated

--- about.php
+++ about.php
@@ -1,89 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <title>Lost and Found</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta name="description" content="" />
-  <meta name="author" content="" />
-
-  <!-- css -->
-  <link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,400italic,700|Open+Sans:300,400,600,700"
-    rel="stylesheet">
-  <link href="css/bootstrap.css" rel="stylesheet" />
-  <link href="css/bootstrap-responsive.css" rel="stylesheet" />
-  <link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
-  <link href="css/jcarousel.css" rel="stylesheet" />
-  <link href="css/flexslider.css" rel="stylesheet" />
-  <link href="css/style.css" rel="stylesheet" />
-  <!-- Theme skin -->
-  <link href="skins/default.css" rel="stylesheet" />
-  <!-- Fav and touch icons -->
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png" />
-  <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png" />
-  <link rel="shortcut icon" href="ico/favicon.png" />
-
-  <!-- =======================================================
-    Theme Name: Flattern
-    Theme URL: https://bootstrapmade.com/flattern-multipurpose-bootstrap-template/
-    Author: BootstrapMade.com
-    Author URL: https://bootstrapmade.com
-  ======================================================= -->
-</head>
+<?php  
+include('links/header.php');       
 
 
-<body>
-  <div id="wrapper">
-    <header style="background-color: #130f40;">
-        <div class="container">
-        <div class="row nomargin">
-          <div class="span12">
-            <div class="headnav" style="margin-right: 13px;" >
-              <ul>
-                <li><a href="logins.php?logout='1' " style="color:red; font-size:15px"><i class="icon-user"></i>Logout</a></li>
-              </ul>
-            </div>   
-          </div>
-        </div>
-          <div class="row">
-            <div class="span4">
-              <div class="logo" style="margin-top: -30px;width: 300px;" >
-                <a href="homepage.php"><img src="img/logo3.png" alt="" class="logo" /></a>
-              </div>
-            </div>
-    
-            
-            <div class="span8">
-              <div class="navbar navbar-static-top">
-                <div class="navigation">
-                  <nav>
-                    <ul class="nav topnav">
-                      <li class="dropdown active">
-                        <a href="homepage.php">Home</a>
-                      </li>
-                      <li class="dropdown active">
-                        <a href="about.php"><strong> About Us</strong></a>
-                      </li>
-                      <li class="dropdown active">
-                        <a href="#"><strong>How it works</strong> </a>
-                      </li>
-                  
-                      <li class="dropdown active">
-                        <a href="contact.php"><strong>Support</strong> </a>
-                      </li>
-                    </ul>
-                  </nav>
-                </div>
-                <!-- end navigation -->
-              </div>
-            </div>
-          </div>
-        </div>
-      
-      </header>
+?>
 
       <section id="inner-headline" style="margin-top: -20px;">
         <div class="container">
@@ -166,67 +85,12 @@
     </section>
       
       
-    <footer>
-      <div class="container">
-        <div class="row">
-          
-          <div class="aligncenter span3">
-            <div class="widget">
-              <h5 class="widgetheading">Get in touch with us</h5>
-              <address>
-                <strong>Bidesh Biswas Biki</strong><br>
-                 Daffodil International University<br>
-                Dhaka,Bangladesh
-               </address>
-              <p>
-                <i class="icon-phone"></i> (+880) 1960961558 <br>
-                <i class="icon-envelope-alt"></i> bidesh15-8550@diu.edu.bd
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div id="sub-footer">
-        <div class="container">
-          <div class="row">
-            <div class="span6">
-              <div class="copyright">
-                <p>
-                  <span>&copy; Bidesh Biswas Biki - All right reserved.</span>
-                </p>
-                <div class="credits">
-                  <!--
-                    All the links in the footer should remain intact.
-                    You can delete the links only if you purchased the pro version.
-                    Licensing information: https://bootstrapmade.com/license/
-                    Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Flattern
-                  -->
-                  Designed by <a href="https://sites.google.com/diu.edu.bd/bideshbiswas/home">Bidesh Biswas Biki</a>
-                </div>
-              </div>
-            </div>
-            <div class="span6">
-              <ul class="social-network">
-                <li><a href="#" data-placement="bottom" title="Facebook"><i class="icon-facebook icon-square"></i></a></li>
-                <li><a href="#" data-placement="bottom" title="Twitter"><i class="icon-twitter icon-square"></i></a></li>
-                <li><a href="#" data-placement="bottom" title="Linkedin"><i class="icon-linkedin icon-square"></i></a></li>
-                <li><a href="#" data-placement="bottom" title="Pinterest"><i class="icon-pinterest icon-square"></i></a></li>
-                <li><a href="#" data-placement="bottom" title="Google plus"><i class="icon-google-plus icon-square"></i></a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+    <?php  
+include('links/footer.php');       
+
+
+?>
 
 </div>
-
-
-
-
-
-
-
-
 
 </body>
---
+++ links/footer.php
@@ -0,0 +1,52 @@
+<footer>
+      <div class="container">
+        <div class="row">
+          
+          <div class="aligncenter span3">
+            <div class="widget">
+              <h5 class="widgetheading">Get in touch with us</h5>
+              <address>
+								<strong>Bidesh Biswas Biki</strong><br>
+								 Daffodil International University<br>
+								Dhaka,Bangladesh
+					 		</address>
+              <p>
+                <i class="icon-phone"></i> (+880) 1960961558 <br>
+                <i class="icon-envelope-alt"></i> bidesh15-8550@diu.edu.bd
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="sub-footer">
+        <div class="container">
+          <div class="row">
+            <div class="span6">
+              <div class="copyright">
+                <p>
+                  <span>&copy; Bidesh Biswas Biki - All right reserved.</span>
+                </p>
+                <div class="credits">
+                  <!--
+                    All the links in the footer should remain intact.
+                    You can delete the links only if you purchased the pro version.
+                    Licensing information: https://bootstrapmade.com/license/
+                    Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Flattern
+                  -->
+                  Designed by <a href="https://sites.google.com/diu.edu.bd/bideshbiswas/home">Bidesh Biswas Biki</a>
+                </div>
+              </div>
+            </div>
+            <div class="span6">
+              <ul class="social-network">
+                <li><a href="#" data-placement="bottom" title="Facebook"><i class="icon-facebook icon-square"></i></a></li>
+                <li><a href="#" data-placement="bottom" title="Twitter"><i class="icon-twitter icon-square"></i></a></li>
+                <li><a href="#" data-placement="bottom" title="Linkedin"><i class="icon-linkedin icon-square"></i></a></li>
+                <li><a href="#" data-placement="bottom" title="Pinterest"><i class="icon-pinterest icon-square"></i></a></li>
+                <li><a href="#" data-placement="bottom" title="Google plus"><i class="icon-google-plus icon-square"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
