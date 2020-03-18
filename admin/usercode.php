48d147f7b21877c4b7bfeda151bfcddbd964762c
Bidesh15-8550 <bideshbiswas@ymail.com>
2020-03-18 18:18:53 +0600

    users from admin section updated

--- admin/usercode.php
+++ admin/usercode.php
@@ -40,28 +40,7 @@ if(isset($_POST['usersbtn']))
 
 
 
-if(isset($_POST['updatebtn']))
-{   
-    $id = $_POST['edit_id'];
-    $username = $_POST['edit_username'];
-    $email = $_POST['edit_email'];
-    $password = $_POST['edit_password'];
-    
-    $query = "UPDATE users SET username = '$username' , email = '$email' , password='$password' WHERE id='$id' ";
-    $query_run= mysqli_query($connetion,$query);
 
-    if($query_run)
-    {
-        $_SESSION['success'] = "Your Data is Updated";
-        header('Location: users.php');
-    }
-    else
-    {
-        $_SESSION['status'] = "Your Data is NOT Updated";
-        header('Location: users.php');
-    }
-
-}
 
 
 
--- admin/users.php
+++ admin/users.php
@@ -90,8 +90,6 @@ include('includes/navbar.php');
             <th>ID</th>
             <th>Username</th>
             <th>Email</th>
-            <th>Password</th>
-            <th>Edit</th>
             <th>Delete</th>
           </tr>
         </thead>
@@ -106,13 +104,8 @@ include('includes/navbar.php');
             <td><?php echo $row['id'];   ?></td>
             <td><?php echo $row['username'];   ?></td> 
             <td><?php echo $row['email'];   ?></td>
-            <td><?php echo $row['password'];   ?></td> 
-            <td>
-                <form action="user_edit.php" method="post">
-                     <input type="hidden" name="edit_id" value="<?php echo $row['id'];   ?>">
-                     <button type="submit" name="edit_btn" class="btn btn-success">Edit</button>
-                </form>
-            </td>
+           
+            
             <td>
                 <form action="usercode.php" method="POST">
                   <input type="hidden" name="delete_id" value="<?php echo $row['id']; ?>">
