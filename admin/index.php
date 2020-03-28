7c979f845a0896e6f501ab520048252ce1d46b1c
Bidesh15-8550 <bideshbiswas@ymail.com>
2020-03-28 11:10:17 +0600

    updated

--- admin/index.php
+++ admin/index.php
@@ -12,7 +12,7 @@ $link=mysqli_connect("localhost","root","");
 mysqli_select_db($link,"adminpanel");
 ?>
 
-    <div id="content-wrapper" class="d-flex flex-column">
+    <div id="content-wrapper" class="d-flex">
       <div id="content">
 
         
@@ -24,14 +24,15 @@ mysqli_select_db($link,"adminpanel");
            
           </div>
 
-        <?php
+      
+            <div class="row">
+            <?php
 
-            $res=mysqli_query($link,"select * from items");
-            while($row=mysqli_fetch_array($res)){
-                ?>
-            
+$res=mysqli_query($link,"select * from items");
+while($row=mysqli_fetch_array($res)){
+    ?>
             <div class="col-sm-4">
-            <div class="product-image-wrapper">
+                <div class="product-image-wrapper">
                 <div class="single-products">
                     <div class="productinfo text-center">
                         <img src="../admin/<?php echo $row["item_image"];    ?>" alt=""height="320"width="250">
@@ -39,8 +40,8 @@ mysqli_select_db($link,"adminpanel");
                         <p>Category: <?php echo $row["item_category"];  ?></p>
                     </div>
                 
-                
-            </div>
+                </div> 
+          </div>
           </div>
 
 
