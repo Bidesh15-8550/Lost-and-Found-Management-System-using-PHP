7d1fbb5bbabac677f22d25d00db1cdba457e28eb
Bidesh15-8550 <bideshbiswas@ymail.com>
2020-03-30 00:49:41 +0600

    users can see their lost products

--- products.php
+++ products.php
@@ -7,10 +7,54 @@ if(!isset($_SESSION['username'])){
 ?>
 <?php  
 include('links/header.php');       
-
-
+$link=mysqli_connect("localhost","root","");
+mysqli_select_db($link,"adminpanel");
 ?>
-   
+   <div id="content-wrapper" class="d-flex">
+      <div id="content">
+
+        
+        <div class="container-fluid">
+
+         
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Lost items</h1>
+           
+          </div>
+
+      
+            <div class="row">
+            <?php
+
+$res=mysqli_query($link,"select * from items");
+while($row=mysqli_fetch_array($res)){
+    ?>
+            <div class="col-sm-4">
+                <div class="product-image-wrapper">
+                <div class="single-products">
+                    <div class="productinfo text-center">
+                        <img src="../Lost and found/admin/<?php echo $row["item_image"];    ?>" alt=""height="320"width="250">
+                        <p>Item Name: <?php echo $row["item_name"];  ?></p>
+                        <p>Category: <?php echo $row["item_category"];  ?></p>
+                    </div>
+                
+                </div> 
+          </div>
+          </div>
+
+
+            <?php
+                }
+        ?>
+        
+     </div>
+
+</div>
+        </div>
+      
+      </div>
+      
+    </div>
 
 
 
